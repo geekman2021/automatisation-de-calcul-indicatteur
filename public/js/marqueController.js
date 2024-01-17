4f672b31aa78652895148e5480ab6586370d6cf3
RakMan <rakotonirinaarnaud@108@gmail.com>
2024-01-17 13:35:10 +0300

    remove comments 2.0

--- public/js/marqueController.js
+++ public/js/marqueController.js
@@ -1,59 +1,50 @@
-const connexe= require('../db/connexion');
+const connexe = require("../db/connexion");
 
+exports.formList = (req, res) => {
+  let sql = `SELECT * FROM marque ORDER BY code_marque`;
+  connexe.query(sql, (err, result) => {
+    if (err) throw err;
 
-
-exports.formList= (req, res)=>{
-    let sql=`SELECT * FROM marque ORDER BY code_marque`;
-   connexe.query(sql, (err, result)=>{
-       if(err) throw err;
-    //    if (req.session.success){
-    //     res.locals.success = req.session.success;
-    //     req.session.success= undefined;
-    // }
-
-
-       console.log(req.session);
-        res.render('pages/marque', {
-            list_marques: result
+    console.log(req.session);
+    res.render("pages/marque", {
+      list_marques: result,
     });
-   });
+  });
 };
 
-exports.ajoutMarque= (req, res)=>{
-    let sql= 'INSERT INTO marque SET ?';
-    let post= {
-        code_marque: req.body.code_marque,
-        nom_marque: req.body.nom_marque
-    };
+exports.ajoutMarque = (req, res) => {
+  let sql = "INSERT INTO marque SET ?";
+  let post = {
+    code_marque: req.body.code_marque,
+    nom_marque: req.body.nom_marque,
+  };
 
-    connexe.query(sql, post, (err, result)=>{
-        if(err) throw err;
-        req.flash('success', 'Ajouter avec succès');
-        res.redirect('marque'); 
-    });
+  connexe.query(sql, post, (err, result) => {
+    if (err) throw err;
+    req.flash("success", "Ajouter avec succès");
+    res.redirect("marque");
+  });
 };
 
-exports.modifMarque= (req, res)=>{
-    let post={
-        code_marque: req.body.code_marqueModif1,
-        nom_marque: req.body.nom_marqueModif,
-        
-    };
-    let sql= ` UPDATE  marque SET ? WHERE code_marque= '${req.body.code_marqueModif}' `;
-    connexe.query(sql, post,  (err, result)=>{
-        if (err) throw err;
-        res.redirect('/app/marque');
-    });
+exports.modifMarque = (req, res) => {
+  let post = {
+    code_marque: req.body.code_marqueModif1,
+    nom_marque: req.body.nom_marqueModif,
+  };
+  let sql = ` UPDATE  marque SET ? WHERE code_marque= '${req.body.code_marqueModif}' `;
+  connexe.query(sql, post, (err, result) => {
+    if (err) throw err;
+    res.redirect("/app/marque");
+  });
 };
 
-exports.suppMarque= (req, res)=>{
-    let post= {
-        code_marque: req.body.marque_supp
-    };
-    let sql= ` DELETE FROM marque WHERE code_marque= '${post.code_marque}' `;
-    connexe.query(sql, (err, result)=>{
-        if (err) throw err;
-        res.redirect('/app/marque');
-
-    });
-}
+exports.suppMarque = (req, res) => {
+  let post = {
+    code_marque: req.body.marque_supp,
+  };
+  let sql = ` DELETE FROM marque WHERE code_marque= '${post.code_marque}' `;
+  connexe.query(sql, (err, result) => {
+    if (err) throw err;
+    res.redirect("/app/marque");
+  });
+};
--- public/js/vehiculeController.js
+++ public/js/vehiculeController.js
@@ -1,103 +1,99 @@
-const connexe= require('../db/connexion');
+const connexe = require("../db/connexion");
 
-exports.formList= (req, res)=>{
-    let sql= `SELECT vehicule.ID, marque.nom_marque, vehicule.Lot, genre.lot_genre, proprietaire.nom, vehicule.num_immat, vehicule.nbre_place, vehicule.charge_utile, vehicule.poid_vide, vehicule.energie, (YEAR(date_entree)-(YEAR(date_sortie))) as age, date_entree, date_sortie FROM vehicule,marque,genre, proprietaire WHERE vehicule.code_marque=marque.code_marque AND vehicule.code_genre=genre.code_genre AND  vehicule.id_proprietaire=proprietaire.id_proprietaire ORDER BY vehicule.num_immat DESC LIMIT 0, 100`;
-    let sql1= `SELECT * FROM genre`;
-    let sql3= `SELECT * FROM marque`;
-    let sql4= `SELECT * FROM proprietaire`;
-    //let sql5= `SELECT JSON_OBJECT( 'lot_genre', lot_genre, 'code_genre', code_genre ) AS a FROM genre ORDER BY code_genre`;
-    connexe.query(sql, (err, result)=>{
-        if(err) throw err;
-        connexe.query(sql1, (err1, result1)=>{
-            if (err1) throw err1;
-                connexe.query(sql3, (err3, result3)=>{
-                    if(err3) throw err3;
-                    connexe.query(sql4, (err4, result4)=>{
-                        if(err4) throw err4;
-                            res.render('pages/vehicule', {
-                            list_vehicules: result,
-                            list_genres: result1,
-                            list_marques: result3,
-                            list_proprietaires: result4,
-                        });
-
-                        });
-                    });
-                });
-            });
-
-};
-
-exports.ajoutVehicule= (req, res)=>{
-    let post= {
-        ID: req.body.ID,
-        num_immat: req.body.num_immat,
-        code_genre: req.body.code_genre,
-        code_marque: req.body.code_marque,
-        energie: req.body.code_energie,
-        id_proprietaire: req.body.id_proprietaire,
-        nbre_place: req.body.nbre_place, 
-        charge_utile: req.body.charge_utile,
-        poid_vide: req.body.poid_vide,
-        date_sortie: req.body.date_sortie,
-        date_entree: req.body.date_entree,
-        Lot: req.body.lot
-    };
-    let sql ='INSERT INTO vehicule SET ?';
-    let sql2= `UPDATE vehicule SET age= (YEAR(date_entree)-(YEAR(date_sortie)))  WHERE num_immat='${post.num_immat}' `;
-    connexe.query(sql, post, (err, result)=>{
-        if(err) throw err;
-        connexe.query(sql2, (err, result2)=>{
-            if(err) throw err;
-            req.flash('success', 'Ajouter avec succès');
-            res.redirect('vehicule');
+exports.formList = (req, res) => {
+  let sql = `SELECT vehicule.ID, marque.nom_marque, vehicule.Lot, genre.lot_genre, proprietaire.nom, vehicule.num_immat, vehicule.nbre_place, vehicule.charge_utile, vehicule.poid_vide, vehicule.energie, (YEAR(date_entree)-(YEAR(date_sortie))) as age, date_entree, date_sortie FROM vehicule,marque,genre, proprietaire WHERE vehicule.code_marque=marque.code_marque AND vehicule.code_genre=genre.code_genre AND  vehicule.id_proprietaire=proprietaire.id_proprietaire ORDER BY vehicule.num_immat DESC LIMIT 0, 100`;
+  let sql1 = `SELECT * FROM genre`;
+  let sql3 = `SELECT * FROM marque`;
+  let sql4 = `SELECT * FROM proprietaire`;
+  connexe.query(sql, (err, result) => {
+    if (err) throw err;
+    connexe.query(sql1, (err1, result1) => {
+      if (err1) throw err1;
+      connexe.query(sql3, (err3, result3) => {
+        if (err3) throw err3;
+        connexe.query(sql4, (err4, result4) => {
+          if (err4) throw err4;
+          res.render("pages/vehicule", {
+            list_vehicules: result,
+            list_genres: result1,
+            list_marques: result3,
+            list_proprietaires: result4,
+          });
         });
-});
-
+      });
+    });
+  });
 };
 
-exports.modifVehicule= (req, res)=>{
-    let post= {
-        ID: req.body.IDModif,
-        num_immat: req.body.num_immatModif,
-        code_marque: req.body.code_marqueModif,
-        code_genre: req.body.code_genreModif,
-        energie: req.body.code_energieModif,
-        date_entree: req.body.date_entreeModif,
-        date_sortie: req.body.date_sortieModif,
-        id_proprietaire: req.body.id_proprietaireModif,
-        nbre_place: req.body.nbre_placeModif,
-        charge_utile: req.body.charge_utileModif,
-        poid_vide: req.body.poid_videModif,
-    };
-
-    let sql= `UPDATE vehicule SET ? WHERE ID= '${post.ID}' `;
-
-    connexe.query(sql, post,  (err, result)=>{
-        if (err) throw err;
-        res.redirect('vehicule');
+exports.ajoutVehicule = (req, res) => {
+  let post = {
+    ID: req.body.ID,
+    num_immat: req.body.num_immat,
+    code_genre: req.body.code_genre,
+    code_marque: req.body.code_marque,
+    energie: req.body.code_energie,
+    id_proprietaire: req.body.id_proprietaire,
+    nbre_place: req.body.nbre_place,
+    charge_utile: req.body.charge_utile,
+    poid_vide: req.body.poid_vide,
+    date_sortie: req.body.date_sortie,
+    date_entree: req.body.date_entree,
+    Lot: req.body.lot,
+  };
+  let sql = "INSERT INTO vehicule SET ?";
+  let sql2 = `UPDATE vehicule SET age= (YEAR(date_entree)-(YEAR(date_sortie)))  WHERE num_immat='${post.num_immat}' `;
+  connexe.query(sql, post, (err, result) => {
+    if (err) throw err;
+    connexe.query(sql2, (err, result2) => {
+      if (err) throw err;
+      req.flash("success", "Ajouter avec succès");
+      res.redirect("vehicule");
     });
+  });
 };
 
-exports.suppVehicule= (req, res)=>{
-    let post={
-        ID: req.body.IDSupp,
-        num_immat: req.body.num_immatSupp
-    };
+exports.modifVehicule = (req, res) => {
+  let post = {
+    ID: req.body.IDModif,
+    num_immat: req.body.num_immatModif,
+    code_marque: req.body.code_marqueModif,
+    code_genre: req.body.code_genreModif,
+    energie: req.body.code_energieModif,
+    date_entree: req.body.date_entreeModif,
+    date_sortie: req.body.date_sortieModif,
+    id_proprietaire: req.body.id_proprietaireModif,
+    nbre_place: req.body.nbre_placeModif,
+    charge_utile: req.body.charge_utileModif,
+    poid_vide: req.body.poid_videModif,
+  };
 
-    let sql= `DELETE FROM vehicule WHERE ID= '${post.ID}' `;
+  let sql = `UPDATE vehicule SET ? WHERE ID= '${post.ID}' `;
 
-    connexe.query(sql, (err, result)=>{
-        if(err) throw err;
-        res.redirect('vehicule');
-    });
+  connexe.query(sql, post, (err, result) => {
+    if (err) throw err;
+    res.redirect("vehicule");
+  });
 };
 
-exports.get_info= (req, res)=>{
-    let num_immat= req.body.num
-    let sql=`SELECT proprietaire.nom, proprietaire.profession, proprietaire.adresse, marque.nom_marque,  vehicule.num_immat, DATE_FORMAT(vehicule.date_sortie, "%d %b %Y") AS date_sortie, genre.lot_genre, vehicule.nbre_place, vehicule.poid_vide, vehicule.charge_utile  FROM vehicule, genre, marque, proprietaire WHERE vehicule.code_marque=marque.code_marque AND vehicule.code_genre= genre.code_genre AND proprietaire.id_proprietaire= vehicule.id_proprietaire AND vehicule.num_immat='${num_immat}' `;
-    connexe.query(sql, (err, result)=>{
-        if (err) throw err;
-        res.json({info:result, nb: result.length} );
-    });
-}
+exports.suppVehicule = (req, res) => {
+  let post = {
+    ID: req.body.IDSupp,
+    num_immat: req.body.num_immatSupp,
+  };
+
+  let sql = `DELETE FROM vehicule WHERE ID= '${post.ID}' `;
+
+  connexe.query(sql, (err, result) => {
+    if (err) throw err;
+    res.redirect("vehicule");
+  });
+};
+
+exports.get_info = (req, res) => {
+  let num_immat = req.body.num;
+  let sql = `SELECT proprietaire.nom, proprietaire.profession, proprietaire.adresse, marque.nom_marque,  vehicule.num_immat, DATE_FORMAT(vehicule.date_sortie, "%d %b %Y") AS date_sortie, genre.lot_genre, vehicule.nbre_place, vehicule.poid_vide, vehicule.charge_utile  FROM vehicule, genre, marque, proprietaire WHERE vehicule.code_marque=marque.code_marque AND vehicule.code_genre= genre.code_genre AND proprietaire.id_proprietaire= vehicule.id_proprietaire AND vehicule.num_immat='${num_immat}' `;
+  connexe.query(sql, (err, result) => {
+    if (err) throw err;
+    res.json({ info: result, nb: result.length });
+  });
+};
